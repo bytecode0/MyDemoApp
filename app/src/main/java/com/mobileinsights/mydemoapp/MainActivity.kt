8fde085336653039bf69d55cde3a66d1c49886a2
Luis Vespa <vespasoft@gmail.com>
2023-08-28 00:31:39 +0200

    Delete unused dependencies

--- app/src/main/java/com/mobileinsights/mydemoapp/MainActivity.kt
+++ app/src/main/java/com/mobileinsights/mydemoapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.mobileinsights.mydemoapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -15,10 +14,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -105,7 +102,7 @@ private fun getPermissionMessage(hasPermission: Boolean) : String =
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MyDemoAppPreview() {
     MyDemoAppTheme {
         val context = LocalContext.current
         val permissionChecker = PermissionChecker(context = context)
